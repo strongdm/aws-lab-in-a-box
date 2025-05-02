e74a178d20babddbdebb058130127f6fe63dd2b8
Lance Haig <lance.haig@strongdm.com>
2025-05-02 09:10:12 +0200

    Add comments to the dc module

--- dc/dc.tf
+++ dc/dc.tf
@@ -1,14 +1,31 @@
+#--------------------------------------------------------------
+# Domain Controller Module for StrongDM Integration
+#
+# This module creates a Windows domain controller in AWS that serves as the 
+# foundation for Windows authentication in the StrongDM lab environment.
+# It sets up Active Directory Domain Services (ADDS) and Active Directory
+# Certificate Services (ADCS) to enable certificate-based authentication.
+#
+# Components:
+# - Windows Server EC2 instance configured as domain controller
+# - Certificate Authority for RDP certificate authentication
+# - Required key pairs and security configurations
+#--------------------------------------------------------------
+
+# Create a Windows Server instance to be configured as a domain controller
 resource "aws_instance" "dc" {
   instance_type = "t2.medium"
   ami           = var.ami
 
   user_data_replace_on_change = true
 
-
+  # Enable password retrieval for the Windows administrator account
   get_password_data      = true
   key_name               = aws_key_pair.windows.key_name
   vpc_security_group_ids = [var.sg]
   subnet_id              = var.subnet_id
+  
+  # Deploy the PowerShell script that sets up the domain controller
   user_data              = templatefile("../dc/install-dc.ps1.tpl", {
     name     = var.name
     password = random_password.admin_password.result
@@ -16,33 +33,30 @@ resource "aws_instance" "dc" {
     }
   )
 
+  # Provide sufficient disk space for AD DS and AD CS
   root_block_device {
     volume_size = 40
   }
 
   tags = local.thistagset
-
 }
 
-#TODO: Port to powershell in case this is running on Windows
-#data "external" "rdpcertificate" {
-#    program = ["bash", "${path.module}/windowsrdpca.sh"]
-#}
-
+# Retrieve the RDP CA certificate from StrongDM using the appropriate script
+# based on the operating system (bash for Linux/macOS, PowerShell for Windows)
 data "external" "rdpcertificate" {
     program = [local.interpreter, local.script]
 }
 
-
+# Generate a key pair for secure communication with the Windows instance
 resource "tls_private_key" "windows" {
   algorithm = "RSA"
 }
 
+# Create an AWS key pair using the generated key
 resource "aws_key_pair" "windows" {
   key_name   = "${var.name}-windows-key"
   public_key = tls_private_key.windows.public_key_openssh
   tags = local.thistagset
-
 }
 
 
--- dc/outputs.tf
+++ dc/outputs.tf
@@ -1,35 +1,60 @@
+#--------------------------------------------------------------
+# Domain Controller Module Outputs
+#
+# These outputs expose important information about the domain controller
+# for use by other modules, particularly for configuring Windows targets
+# and StrongDM resources that need domain authentication.
+#--------------------------------------------------------------
+
+# Network information for accessing the domain controller
 output "dc_fqdn" {
     value = aws_instance.dc.private_dns
+    description = "The private DNS name of the domain controller"
 }
 
 output "dc_ip" {
     value = aws_instance.dc.private_ip
+    description = "The private IP address of the domain controller"
 }
 
+# Administrator account credentials for direct RDP access
 output "dc_username" {
     value = "administrator"
+    description = "The local administrator username for the domain controller"
 }
 
 output "dc_password" {
     value = rsadecrypt(aws_instance.dc.password_data, tls_private_key.windows.private_key_pem)
+    description = "The decrypted password for the local administrator account"
+    sensitive = true
 }
 
+# Domain administrator credentials for domain operations
 output "domain_admin" {
     value = "domainadmin"
+    description = "The domain administrator username created during setup"
 }
 
 output "domain_password" {
     value = "${random_password.admin_password.result}!"
+    description = "The password for the domain administrator account"
+    sensitive = true
 }
 
+# Resource tags for consistent resource management
 output "thistagset" {
     value = local.thistagset
+    description = "The tags applied to resources in this module"
 } 
 
+# Key material for other resources that need to join the domain
 output "private_key_pem" {
     value = tls_private_key.windows.private_key_pem
+    description = "The private key in PEM format for decrypting Windows passwords"
+    sensitive = true
 }
 
 output "key_name" {
     value = aws_key_pair.windows.key_name
+    description = "The name of the key pair created for Windows instances"
 }
