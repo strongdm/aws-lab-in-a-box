19645d1d360342ab2915845f34ca76dfaaf731c7
Nico Corrarello <nicolas@corrarello.com>
2025-12-04 09:07:37 +0000

    Allow kpasswd to the domain controller for domain joined linux servers

--- network/network.tf
+++ network/network.tf
@@ -211,6 +211,21 @@ resource "aws_vpc_security_group_ingress_rule" "allow_windows" {
   })
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_kpasswd" {
+  count             = var.create_windows_target || var.create_domain_controller ? 1 : 0
+  security_group_id = aws_security_group.relay.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 464
+  ip_protocol       = "tcp"
+  to_port           = 464
+
+  tags = merge(var.tagset, {
+    network = "Private"
+    Name    = "${var.name}-private-sg"
+  })
+}
+
+
 resource "aws_vpc_security_group_ingress_rule" "allow_hcvault" {
   count             = var.create_hcvault ? 1 : 0
   security_group_id = aws_security_group.relay.id
